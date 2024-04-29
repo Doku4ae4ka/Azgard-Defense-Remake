896e51f60ccf42db8e9d0c6f67913f4195397647
Pavel Dokuchaev <112896012+Doku4ae4ka@users.noreply.github.com>
2024-04-29 17:12:01 +0300

    conversion to ECS
    - Architecture conversion to ECS pattern
    - Some fixes with UI

--- Assets/Scripts/Waves/IWave.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IWave
-{
-    IEnumerator SpawnEnemies(GameObject enemyPrefab, Transform spawnPoint, float delay, int count);
-}
